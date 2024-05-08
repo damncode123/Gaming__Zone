084833948aa65460c3c86966c289a8fdf0cdad6b
damncode123 <abhinavpandeyjan05@gmail.com>
2024-05-08 14:50:24 +0530

    bug

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,15 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: "./.env" });
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 const server = http.createServer(app);
 const io = new Server(server);
+
 
 app.use(express.static(join(__dirname, '../frontend/build')));
 
@@ -25,7 +32,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use(cors());
+
 app.use(express.json());
 app.use(express.static("public"));
 
