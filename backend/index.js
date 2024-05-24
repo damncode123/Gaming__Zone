5449f668bb6649b48be0abe8add7e5c17e4e6def
Abhinav Pandey <134854396+damncode123@users.noreply.github.com>
2024-05-25 00:47:35 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,10 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
 });
 
 const userSocketMap = {};
